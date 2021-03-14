aed209e356e5a9f8d201ddc1ca036ab0bb123a01
alphazhangyz <alphazhangyz@gmail.com>
2021-03-14 16:09:09 +0800

    hash表常见方法

---
+++ HashTable/hashTable.js
@@ -0,0 +1,29 @@
+/**
+ * hash表
+ */
+
+class HashTable {
+  constructor () {
+    this.table = {}
+  }
+
+  // 向hash表增加一项
+  put () {
+
+  }
+
+  hashCode (key) {
+    
+  }
+
+  // 根据键值从hash表中移除一项
+  remove (key) {
+
+  }
+
+  // 返回根据索引得到的值
+  get (key) {
+
+  }
+
+}
